19fc516ae95c446e41a00c02afd577080f10489a
DaBi0212 <davidcrisantosb@gmail.com>
2025-12-05 02:26:33 -0600

    Actualizar API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://davyb.pythonanywhere.com/api/',  // ¡TU BACKEND EN PYTHONANYWHERE!
+  // apiUrl: 'http://localhost:8000/api/',  // Comenta o elimina esta línea
 };
