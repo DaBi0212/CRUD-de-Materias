e778f3a7118a745a5256c156408877b657e6212a
DaBi0212 <davidcrisantosb@gmail.com>
2025-12-05 12:37:13 -0600

    Fix Envoriment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend-crud-de-materias.onrender.com/api/',
-  url_api: 'https://backend-crud-de-materias.onrender.com/api/',
+  // 1. Apuntar a RENDER (https)
+  // 2. QUITAR la barra al final para evitar el doble slash (api/ -> api)
+  apiUrl: 'https://backend-crud-de-materias.onrender.com/api',
+  url_api: 'https://backend-crud-de-materias.onrender.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,6 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000/api/',
-  url_api: 'http://localhost:8000/api/',
+  // QUITAR la barra al final aquí también para mantener consistencia
+  apiUrl: 'http://localhost:8000/api',
+  url_api: 'http://localhost:8000/api'
 };
