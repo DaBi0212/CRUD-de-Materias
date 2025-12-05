38ef04d85cf33ba8b3fb7476c38b010bca6ead48
DaBi0212 <davidcrisantosb@gmail.com>
2025-12-05 11:38:55 -0600

    Fix CORS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,  // Cambiar a true
+  production: true,
   apiUrl: 'https://backend-crud-de-materias.onrender.com/api/',
   url_api: 'https://backend-crud-de-materias.onrender.com/api/',
 };
